e66cc2aaa198d4fc9ceaea8e5ed21de5277d469b
rogh2487 <apple@Apples-MacBook-Air.local>
2024-12-05 15:24:10 +0530

    Allow the add method to handle new lines between numbers (instead of commas). (1n2,3 should return 6)

--- test.ts
+++ test.ts
@@ -2,11 +2,11 @@ function add(numbers: string) : number {
   
   if(numbers === '') return 0;
   
-  let num_arr : string [] = numbers.split(',');
+  let num_arr : string [] = numbers.split(/[,\n]/);
   
   if (num_arr.length == 1) return parseInt(num_arr[0]);
   
   return num_arr.reduce((sum,value)=> sum = sum + parseInt(value),0);
 }
 
-console.log(add('3,4,10,20'))
+console.log(add('1\n2,3'))
