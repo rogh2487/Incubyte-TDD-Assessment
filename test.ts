63dc38de30abb576c0c6d91802aac0fdcff36100
rogh2487 <apple@Apples-MacBook-Air.local>
2024-12-05 15:20:56 +0530

    Allow the add method to handle any amount of numbers.

--- test.ts
+++ test.ts
@@ -1,11 +1,12 @@
-function add(numbers : string) : number {
+function add(numbers: string) : number {
   
-    if(numbers === '') return 0;
-    
-    let num_arr : string [] = numbers.split(',');
-    
-    if (num_arr.length == 1) return parseInt(num_arr[0]);
-    
-  }
+  if(numbers === '') return 0;
   
-  console.log(add('3'))
+  let num_arr : string [] = numbers.split(',');
+  
+  if (num_arr.length == 1) return parseInt(num_arr[0]);
+  
+  return num_arr.reduce((sum,value)=> sum = sum + parseInt(value),0);
+}
+
+console.log(add('3,4,10,20'))
